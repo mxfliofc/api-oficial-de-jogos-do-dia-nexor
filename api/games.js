dcb32c615d2fa28ddc4a7790b32eaa1a216e83d4
Mxflixa Support <126412851+mxfliofc@users.noreply.github.com>
2026-09-23 23:07:14 -0300

    Update games.js

--- api/games.js
+++ api/games.js
@@ -1,19 +1,9 @@
-import games from '../public/games.json' with {
-  type: 'json'
-};
+import games from "../public/games.json" with { type: "json" };
 
-export default function handler(
-  request,
-  response
-) {
+export default function handler(request, response) {
   response.setHeader(
-    'Content-Type',
-    'application/json; charset=utf-8'
-  );
-
-  response.setHeader(
-    'Cache-Control',
-    'public, s-maxage=300, stale-while-revalidate=60'
+    "Cache-Control",
+    "public, s-maxage=300, stale-while-revalidate=60"
   );
 
   response.status(200).json(games);
